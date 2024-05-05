9683625101cf99a984385d083c3766cc2ca8ff8d
M-Stark <55349494+M-Stark@users.noreply.github.com>
2024-05-05 09:23:25 +0600

    update files

--- dfgdf.py
+++ dfgdf.py
@@ -1,18 +1,1 @@
-<<<<<<< HEAD
-dfgdfgdfgdf
-gd
-fg
-dfgdf
-gdfgdfg
-dfs
-gds
-
-
-def 43534534dfgdf
-dgdsfgdfs
-=======
-34543543543
-534
-534
-543534
->>>>>>> 4c1e9fa51876715152a74c67d80ec2bc72580ac3
+ddfdfsdfs
